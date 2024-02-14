bdf69724c666496834b738fa2c1ff352a109d00a
Leandro Nogueira <leandro.nogueir4@gmail.com>
2024-02-14 01:55:47 +0000

    Atualizada a dim_produtos com inclusao de subcategorias e categorias dos produtos

--- models/staging/stg_sap__produtos.sql
+++ models/staging/stg_sap__produtos.sql
@@ -3,6 +3,7 @@ with
         select
             cast(productid as int) as id_produto
             , cast(name as string) as nome_produto
+            , cast(productsubcategoryid as int) as id_pro_subcategoria
         from {{ source('sap_adw', 'product') }}
     )
 select *
