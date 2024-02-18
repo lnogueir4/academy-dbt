05773a96298a645b5047c7f7016bb70e51cbf88f
Leandro Nogueira <leandro.nogueir4@gmail.com>
2024-02-18 16:11:41 +0000

    Ajustada a coluna de estado civil para trocar o M do ingles married para C de casado.

--- models/staging/stg_sap__empregados.sql
+++ models/staging/stg_sap__empregados.sql
@@ -4,7 +4,9 @@ with
             cast(businessentityid as int) as id_empregado
             , cast(jobtitle as string) as cargo
             , date(birthdate) as data_nasc
-            , cast(maritalstatus as string) as estado_civil
+            , case
+                when cast(maritalstatus as string) = 'M' then 'C'
+            end as estado_civil
             , cast(gender as string) as genero
             , date(hiredate) as data_admissao
             , cast(vacationhours as int) as horas_de_ferias
